17a16b8521046f895c38bcbafad211bb5968aadf
Nattapong Nunpan <aixasz@hotmail.com>
2019-06-03 16:03:56 +0700

    Create test base class

---
+++ CustomerInquiry.Tests/CustomerInquiryTestBase.cs
@@ -0,0 +1,62 @@
+﻿using CustomerInquiry.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerInquiry.Tests
+{
+    public abstract class CustomerInquiryTestBase
+    {
+        protected readonly CustomerInquiryContext DbContext;
+
+        protected CustomerInquiryTestBase()
+        {
+            var options = new DbContextOptionsBuilder<CustomerInquiryContext>()
+                .UseInMemoryDatabase(databaseName: "CustomerInquiry")
+                .Options;
+
+            DbContext = new CustomerInquiryContext(options);
+
+            SeedContext(DbContext);
+        }
+
+        private void SeedContext(CustomerInquiryContext dbContext)
+        {
+            var customer = new Customer
+            {
+                CustomerID = 1,
+                Email = "test@test.com",
+                Mobile = "0987654321",
+                Name = "fullname lastname"
+            };
+
+            dbContext.Customers.Add(customer);
+
+            var transactions = new List<Transaction>
+            {
+                new Transaction
+                {
+                    Id = 1,
+                    CustomerID = 1,
+                    Amount = 1500.00m,
+                    Currency = "THB",
+                    Date = DateTime.UtcNow,
+                    Status = "Success"
+                },
+                new Transaction
+                {
+                    Id = 2,
+                    CustomerID = 1,
+                    Amount = 59.99m,
+                    Currency = "USD",
+                    Date = DateTime.UtcNow,
+                    Status = "Failed"
+                }
+            };
+
+            dbContext.Transactions.AddRange(transactions);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ CustomerInquiry.Tests/Services/CustomerInquiryServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using CustomerInquiry.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CustomerInquiry.Tests.Services
+{
+    public class CustomerInquiryServiceTest : CustomerInquiryTestBase
+    {
+        public CustomerInquiryServiceTest()
+        {
+
+        }
+    }
+}
