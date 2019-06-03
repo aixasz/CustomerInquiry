3843f0549c0458892fc715da5a0bd6b233a308e2
Nattapong Nunpan <aixasz@hotmail.com>
2019-06-03 15:36:10 +0700

    renaming project and solution

--- CustomerInquiry/Program.cs
+++ CustomerInquiry/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CustomerInquiry.Api
+namespace CustomerInquiry
 {
     public class Program
     {
--- CustomerInquiry/Startup.cs
+++ CustomerInquiry/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace CustomerInquiry.Api
+namespace CustomerInquiry
 {
     public class Startup
     {
